4733d6c0e831b0d53050028bdc1a127340d3944b
fox86011 <dota2fox86011@gmail.com>
2024-10-21 11:27:00 -0500

    inicial comit

---
+++ clientes1.sql
@@ -0,0 +1,10 @@
+--crear tabla clientes 	
+drop table clientes
+
+create table clientes(
+	cedula char(10) not null,
+	nombre varchar(50) not null,
+	apellido varchar(50) not null,
+	edad int,
+	constraint personas_pk primary key (cedula)
+)
